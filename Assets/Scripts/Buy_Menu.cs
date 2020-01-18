95f84e24e02448091460bf7a9bfe31f55288ad51
FX <31199346+fx46@users.noreply.github.com>
2020-01-18 11:03:57 -0500

    New Buy_Menu script.

---
+++ Assets/Scripts/Buy_Menu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Character_Enum;
+
+public class Buy_Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void On_Buy_Button(Character_Type Type)
+    {
+        Debug.Log(Type);
+    }
+}
