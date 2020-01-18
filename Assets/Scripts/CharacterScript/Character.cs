e3f98402f81c4a88e3ace343ad334b067c8c08b0
Jacob <nadjam@nad.qc.ca>
2020-01-18 00:57:22 -0500

    Ajout base class character

---
+++ Assets/Scripts/CharacterScript/Character.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    public bool is_on_board;
+
+    public Stat health;
+    public Stat mana;
+    public Stat energy;
+    public Stat energy_remover;
+    public Stat att_damage;
+    public Stat att_speed;
+    public Stat crit_chance;
+    public Stat crit_damage_mult;
+    public Stat lifesteal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Reset_round()
+    {
+        health.curr = health.Actual();
+        mana.curr = 0;
+        energy.curr -= energy_remover.curr;
+    }
+
+    void Take_damage(float dmg)
+    {
+        health.curr -= dmg;
+        health.curr = health.curr> 0 ? health.curr : 0;
+    }
+
+    bool Is_alive()
+    {
+        return (health.curr > 0);
+    }
+
+    float Calculate_damage()
+    {
+        return (Random.Range(0.0f, 1.0f) < crit_chance.curr) ? (int)((float)att_damage.curr * crit_damage_mult.curr) : att_damage.curr;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/CharacterScript/stat.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Stat
+{
+    public float basic;
+    [System.NonSerialized]
+    public float curr;
+    private float actual;
+    private List<float> _bonus_mult;
+    private List<float> _bonus_raw;
+
+    public Stat(float basic_val = 0.0f)
+    {
+        set_val(basic_val);
+    }
+
+    void set_val(float val)
+    {
+        basic = val;
+        curr = val;
+        actual = val;
+        _bonus_mult = new List<float>();
+        _bonus_raw = new List<float>();
+    }
+
+    public Stat Add_mult(float val)
+    {
+        _bonus_mult.Add(val);
+        return calc_actual();
+    }
+
+    public Stat Add_raw(float val)
+    {
+        _bonus_raw.Add(val);
+        return calc_actual();
+    }
+
+    public void Remove_mult(float val)
+    {
+        _bonus_mult.Remove(val);
+        calc_actual();
+    }
+
+    public void Remove_raw(float val)
+    {
+        _bonus_raw.Remove(val);
+        calc_actual();
+    }
+
+    private Stat calc_actual()
+    {
+        float mult = 1; // mult by 1 stays the same
+        for (int i = 0; i < _bonus_mult.Count; ++i)
+        {
+            mult += _bonus_mult[i];
+        }
+
+        // raw bonus
+        float sum = 0;
+        for (int i = 0; i < _bonus_raw.Count; ++i)
+        {
+            sum += _bonus_raw[i];
+        }
+
+        actual = mult * basic + sum;
+        curr += bonus();
+        return this;
+    }
+
+    public float Actual()
+    {
+        return actual;
+    }
+
+    public float bonus()
+    {
+        return actual - basic;
+    }
+}
