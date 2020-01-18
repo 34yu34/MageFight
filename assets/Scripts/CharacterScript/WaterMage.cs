b2d46cba535bf040edd78d2d617318c8e0fe497c
Billy <nadjam@nad.qc.ca>
2020-01-18 04:31:42 -0500

    adds the water_mage prefab

---
+++ assets/Scripts/CharacterScript/WaterMage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterMage : Character
+{
+    public WaterMage()
+    {
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set_val(); uncomment me plz
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public override void Launch_ability()
+    {
+
+    }
+}
