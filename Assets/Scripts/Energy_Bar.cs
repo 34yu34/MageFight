8d22ef945d403b20b73d98fc6528bd9c48bc3626
Audrey Germain <audrey.germain@polymtl.ca>
2020-01-18 17:16:46 -0500

    add energy, mana and health bar for Earth mage

---
+++ Assets/Scripts/Energy_Bar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Energy_Bar : MonoBehaviour
+{
+    Vector3 localScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        localScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        localScale.y = GetComponentInParent<Character>().energy.curr / 1000;
+        transform.localScale = localScale;
+    }
+}
---
+++ Assets/Scripts/Health_Bar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health_Bar : MonoBehaviour
+{
+    Vector3 localScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        localScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        localScale.y = GetComponentInParent<Character>().health.curr / 1000;
+        transform.localScale = localScale;
+    }
+}
