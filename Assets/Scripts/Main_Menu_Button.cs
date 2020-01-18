0d5920b59cd46fa2636b86a3ee2385ba563088bd
Jared <nadjam@nad.qc.ca>
2020-01-18 16:16:35 -0500

    Main menu finished

---
+++ Assets/Scripts/Main_Menu_Button.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu_Button : MonoBehaviour
+{
+
+    public Button play, quit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        play.onClick.AddListener(Play);
+        quit.onClick.AddListener(Quit);
+    }
+
+    void Play()
+    {
+        SceneManager.LoadScene("GameBoard");
+    }
+
+    void Quit()
+    {
+        Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
